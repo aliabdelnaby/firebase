1d421af0a72744fa2cdcae1f352d0218ba8886c6
aliabdelnaby <aliabdelnabi239@gmail.com>
2024-05-07 00:23:18 +0300

    Cloud Messaging Token with Api notification

--- lib/features/test_notification.dart
+++ lib/features/test_notification.dart
@@ -1,6 +1,9 @@
+import 'dart:convert';
+
 import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
 
 class TestNotification extends StatefulWidget {
   const TestNotification({super.key});
@@ -57,7 +60,53 @@ class _TestNotificationState extends State<TestNotification> {
       appBar: AppBar(
         title: const Text("Notification"),
       ),
-      body: Container(),
+      body: Container(
+        alignment: Alignment.topCenter,
+        padding: const EdgeInsets.symmetric(vertical: 200),
+        child: TextButton.icon(
+          onPressed: () async {
+            await sendNotification("Test", "done!!!!");
+          },
+          icon: const Icon(Icons.send),
+          label: const Text("Send Notification"),
+        ),
+      ),
     );
+  }
+}
+
+sendNotification(title, message) async {
+  var headersList = {
+    'Accept': '*/*',
+    'Content-Type': 'application/json',
+    'Authorization':
+        'key=AAAAaX9G6R0:APA91bG9MX0lfn-BNZdTQHAm_bYsXtju-B5y4TQzSwc3yv5HqYiTU2HO16ipF2HQ6Sp_MpkEXpvIBNOuAwHTCUei6bUdTkY94AV1cSIpfjDxNJGErPghTfb_GVBeOyy-2NrkXMW9v5aQ'
+  };
+  var url = Uri.parse('https://fcm.googleapis.com/fcm/send');
+
+  var body = {
+    "to":
+        "f469SCStRwKb_gCc3f78oZ:APA91bHyvsdvQbiS-go1ZuN06dwXsbQeo1y2Ky6s17DcwO5S2bZLmLY-4x6hlVp-rtwVc9AifJBraFVbIhXVv4MhKucvu2AK50U8f4onJYJb-qhgUpJxSl77qd3q5i2IXh_L4qRkqhIJ",
+    "notification": {
+      "title": title,
+      "body": message,
+    }
+  };
+
+  var req = http.Request('POST', url);
+  req.headers.addAll(headersList);
+  req.body = json.encode(body);
+
+  var res = await req.send();
+  final resBody = await res.stream.bytesToString();
+
+  if (res.statusCode >= 200 && res.statusCode < 300) {
+    if (kDebugMode) {
+      print(resBody);
+    }
+  } else {
+    if (kDebugMode) {
+      print(res.reasonPhrase);
+    }
   }
 }
