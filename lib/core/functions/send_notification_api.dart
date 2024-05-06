513ebf5830e733d74345ed6a2fb9a6ed1939886b
aliabdelnaby <aliabdelnabi239@gmail.com>
2024-05-07 01:00:25 +0300

    Refactor sendNotificationForeground Method and sendNotificationAPIRequest Method

---
+++ lib/core/functions/send_notification_api.dart
@@ -0,0 +1,45 @@
+import 'dart:convert';
+import 'package:http/http.dart' as http;
+import 'package:flutter/foundation.dart';
+
+sendNotificationAPIRequest(title, message) async {
+  var headersList = {
+    'Accept': '*/*',
+    'Content-Type': 'application/json',
+    'Authorization':
+        'key=AAAAaX9G6R0:APA91bG9MX0lfn-BNZdTQHAm_bYsXtju-B5y4TQzSwc3yv5HqYiTU2HO16ipF2HQ6Sp_MpkEXpvIBNOuAwHTCUei6bUdTkY94AV1cSIpfjDxNJGErPghTfb_GVBeOyy-2NrkXMW9v5aQ'
+  };
+  var url = Uri.parse('https://fcm.googleapis.com/fcm/send');
+
+  var body = {
+    "to":
+        "f469SCStRwKb_gCc3f78oZ:APA91bHyvsdvQbiS-go1ZuN06dwXsbQeo1y2Ky6s17DcwO5S2bZLmLY-4x6hlVp-rtwVc9AifJBraFVbIhXVv4MhKucvu2AK50U8f4onJYJb-qhgUpJxSl77qd3q5i2IXh_L4qRkqhIJ",
+    "notification": {
+      "title": title,
+      "body": message,
+    }
+  };
+
+  var req = http.Request('POST', url);
+  req.headers.addAll(headersList);
+  req.body = json.encode(body);
+
+  var res = await req.send();
+  final resBody = await res.stream.bytesToString();
+
+  if (res.statusCode >= 200 && res.statusCode < 300) {
+    if (kDebugMode) {
+      print(resBody);
+    }
+    if (kDebugMode) {
+      print("Sueccessful notification send");
+    }
+  } else {
+    if (kDebugMode) {
+      print(res.reasonPhrase);
+    }
+    if (kDebugMode) {
+      print("Failed notification send");
+    }
+  }
+}
--- lib/features/test_notification.dart
+++ lib/features/test_notification.dart
@@ -1,10 +1,7 @@
-import 'dart:convert';
-
-import 'package:firebase_app/core/functions/send_notification_foreground.dart';
+import 'package:firebase_app/core/functions/send_notification_api.dart';
 import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
-import 'package:http/http.dart' as http;
 
 class TestNotification extends StatefulWidget {
   const TestNotification({super.key});
@@ -20,6 +17,34 @@ class _TestNotificationState extends State<TestNotification> {
     getToken();
     requestPermissionNotification();
     super.initState();
+  }
+
+  void sendNotificationForeground() {
+    FirebaseMessaging.onMessage.listen(
+      (RemoteMessage message) {
+        if (message.notification != null) {
+          if (kDebugMode) {
+            print('Got a message whilst in the foreground!');
+          }
+          if (kDebugMode) {
+            print(
+                'Message also contained a notification: ${message.notification}');
+            print('====== ${message.notification!.title}');
+            print('====== ${message.notification!.body}');
+          }
+          //! Add Custom Notification here like (Snackbar)
+
+          ScaffoldMessenger.of(context).showSnackBar(
+            SnackBar(
+              content: Text(
+                '${message.notification!.title} , ${message.notification!.body}',
+              ),
+              backgroundColor: Colors.orange,
+            ),
+          );
+        }
+      },
+    );
   }
 
   getToken() async {
@@ -67,54 +92,12 @@ class _TestNotificationState extends State<TestNotification> {
         padding: const EdgeInsets.symmetric(vertical: 200),
         child: TextButton.icon(
           onPressed: () async {
-            await sendNotification("Test", "done!!!!");
+            await sendNotificationAPIRequest("Test", "done!!!!");
           },
           icon: const Icon(Icons.send),
           label: const Text("Send Notification"),
         ),
       ),
     );
-  }
-}
-
-sendNotification(title, message) async {
-  var headersList = {
-    'Accept': '*/*',
-    'Content-Type': 'application/json',
-    'Authorization':
-        'key=AAAAaX9G6R0:APA91bG9MX0lfn-BNZdTQHAm_bYsXtju-B5y4TQzSwc3yv5HqYiTU2HO16ipF2HQ6Sp_MpkEXpvIBNOuAwHTCUei6bUdTkY94AV1cSIpfjDxNJGErPghTfb_GVBeOyy-2NrkXMW9v5aQ'
-  };
-  var url = Uri.parse('https://fcm.googleapis.com/fcm/send');
-
-  var body = {
-    "to":
-        "f469SCStRwKb_gCc3f78oZ:APA91bHyvsdvQbiS-go1ZuN06dwXsbQeo1y2Ky6s17DcwO5S2bZLmLY-4x6hlVp-rtwVc9AifJBraFVbIhXVv4MhKucvu2AK50U8f4onJYJb-qhgUpJxSl77qd3q5i2IXh_L4qRkqhIJ",
-    "notification": {
-      "title": title,
-      "body": message,
-    }
-  };
-
-  var req = http.Request('POST', url);
-  req.headers.addAll(headersList);
-  req.body = json.encode(body);
-
-  var res = await req.send();
-  final resBody = await res.stream.bytesToString();
-
-  if (res.statusCode >= 200 && res.statusCode < 300) {
-    if (kDebugMode) {
-      print(resBody);
-    }
-    if (kDebugMode) {
-      print("Sueccessful notification send");
-    }
-  } else {
-    if (kDebugMode) {
-      print(res.reasonPhrase);
-    }
-    if (kDebugMode) {
-      print("Failed notification send");
-    }
   }
 }
